e71c0f63d43a3033b070c79af86036a2fe05e66f
doublez <1012061740@qq.com>
2021-07-19 22:54:16 +0800

    put

---
+++ ddmall_management/src/main/java/pro/doublez/management/ManagementApplication.java
@@ -0,0 +1,17 @@
+package pro.doublez.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class ManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+
+}
